b47a93d1cca54976052e0dc893db65ccd138a101
ahmedbltagy <ahmed.bltagy@edigits.net>
2023-09-17 10:42:19 +0300

    test

--- app/Product.php
+++ app/Product.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model implements TranslatableContract
 {
-    use Translatable;
+    use Translatable, SoftDeletes;
 
     public $translatedAttributes = [
         'name'
